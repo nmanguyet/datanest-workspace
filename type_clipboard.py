09d8b9bc42649374f0ef7fa9948831a24c87bf57
nmanguyet <nmanhnguyet@gmail.com>
2026-07-15 22:31:49 +0700

    add code typing from clipboard

---
+++ type_clipboard.py
@@ -0,0 +1,19 @@
+#%%
+import subprocess
+import time
+
+text = subprocess.check_output(
+    ["xclip", "-selection", "clipboard", "-o"],
+    text=True,
+)
+
+print("Switch to remote window...")
+time.sleep(5)
+
+for ch in text:
+    if ch == "\n":
+        subprocess.run(["xdotool", "key", "Return"])
+    elif ch == "\t":
+       subprocess.run(["xdotool", "key", "Tab"])
+    else:
+        subprocess.run(["xdotool", "type", "--delay", "10", "--", ch])
